a3626481ec38b8f5bcdd91ec301814ce2a751bbd
Gabe Leon <gableonn@gmail.com>
2021-01-19 11:03:57 -0300

    Init: Started ticket apps

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,14 @@ const router = new Router();
 const User = require('./User');
 const Equipment = require('./Equipment');
 const auth = require('./middlewares/auth');
+const Ticket = require('./Ticket');
 
 router.post('/login', User.Controller.login);
+
 router.use('/users', auth, User.routes);
 
 router.use('/equipments', auth, Equipment.routes);
+
+router.use('/tickets', auth, Ticket.routes);
 
 module.exports = router;
